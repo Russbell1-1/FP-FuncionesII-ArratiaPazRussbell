48402cff408858c37c03290a579ac92f7461fbee
Russbell1-1 <zvalkiria@outlook.es>
2025-06-15 23:01:46 -0500

    Añade Ejercicio 2 final

--- Ejercicio 2.cpp
+++ Ejercicio 2.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 int main(){
-	srand(time(0))
+	srand(time(0));
 	int NumeroAleatorio,CantidadDeNumeros,Rango;
 	cout<<"Ingrese la Cantidad de numeros deseados: ";
-	cin>>CantidadDeNumeros
+	cin>>CantidadDeNumeros;
 	cout<<"Ingrese el valor maximo deseado: ";
 	cin>>Rango;
 	for (int i=1;i<=CantidadDeNumeros;i++){
 		NumeroAleatorio = (rand() % Rango) + 1;
-		cout<<NumeroAleatorio;
+		cout<<NumeroAleatorio<<" ";
 	}
 	return 0;
 	
