24e5a039f4a7f2d7bfb23d9b792635a24b5febf1
Russbell1-1 <zvalkiria@outlook.es>
2025-06-15 22:40:53 -0500

    Añade ejercicio 1 Final

--- Ejercicio 1.cpp
+++ Ejercicio 1.cpp
@@ -5,7 +5,10 @@
 using namespace std;
 
 int main(){
-	float radio;
-	cout<<M_PI;
+	float radio,Areadelcirculo;
+	cout<<"Indique el radio del circulo: ";
+	cin>>radio;
+	Areadelcirculo = pow(radio, 2) * M_PI;
+	cout<<"El area del circulo es: "<<Areadelcirculo<<endl;
 	return 0;
 }
